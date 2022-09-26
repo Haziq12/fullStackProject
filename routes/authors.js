058b46f97505f1f2a9407aeee6ec9e40103d2489
Haziq Naeem <haziqnaeem36@gmail.com>
2022-09-26 19:07:38 -0400

    create Author model, import into routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 // GET All Authors Route
 router.get('/', (req, res) => {
@@ -8,7 +9,7 @@ router.get('/', (req, res) => {
 
 // GET New Author Route
 router.get('/new', (req, res) => {
-  res.render('authors/new')
+  res.render('authors/new', { author: new Author() })
 })
 
 // CREATE Author Route
