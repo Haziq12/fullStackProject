bd3f315c2da95fbae381e609f51470b2d99e7eb4
Haziq Naeem <haziqnaeem36@gmail.com>
2022-09-26 17:20:08 -0400

    create server and mvc structure

--- server.js
+++ server.js
@@ -3,4 +3,9 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 app.set('view enginer', 'ejs')
-app.set('.views', '__dirname + '/views)
+app.set('.views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public')) 
+
+app.listen(process.env.PORT || 3000)
