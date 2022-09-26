b0f4dbc9eef1a05b0d5e0246567ff5d506a6259c
Haziq Naeem <haziqnaeem36@gmail.com>
2022-09-26 19:35:50 -0400

    create post route with new author

--- routes/authors.js
+++ routes/authors.js
@@ -14,7 +14,20 @@ router.get('/new', (req, res) => {
 
 // CREATE Author Route
 router.post('/', (req, res) => {
-  res.send('Create')
+  const author = new Author({
+    name: req.body.name,
+  })
+  author.save((err, newAuthor) => {
+    if(err) {
+      res.render('authors/new', {
+        author: author,
+        errorMessage: 'Error creating author'
+      })
+    } else {
+      // res.redirect(`authors/${newAuthor.id}`)
+      res.redirect('authors')
+    }
+  })
 })
 
 module.exports = router 
